03f20bdabde8cb38f648f488c3d321c1eb084bc7
aleixgarciagispert <aleixgarciagispert@gmail.com>
2026-09-18 09:39:34 +0200

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import SiteLayout from './layouts/SiteLayout';
 import LandingPage from './pages/LandingPage';
 import FrontendPortfolioPage from './pages/FrontendPortfolioPage';
@@ -7,13 +8,16 @@ import ContactPage from './pages/ContactPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SiteLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="front-end" element={<FrontendPortfolioPage />} />
-        <Route path="contact" element={<ContactPage />} />
-      </Route>
-      <Route path="3d-environments" element={<CgiReelPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SiteLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="front-end" element={<FrontendPortfolioPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+        <Route path="3d-environments" element={<CgiReelPage />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
